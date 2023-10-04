e4f896c3a9bfda19c19dd463f9d571202c5dcf01
Andressa Costa <emailandressacosta@gmail.com>
2023-10-04 20:10:47 -0300

    first commit

---
+++ src/ParametrosInvalidosException.java
@@ -0,0 +1,15 @@
+
+
+/*public class ParametrosInvalidosException {
+
+    public static void main (String [] args, int parametroUm, int parametroDois) {
+        if (parametroUm > parametroDois){
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+    }
+
+}
+}*/
+
+public class ParametrosInvalidosException extends Exception {
+    
+}
